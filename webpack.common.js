89c55d414dca20721369503a12f87841466fca0f
airasyaqub <airas.yaqub@bramerz.pk>
2020-12-13 17:24:59 +0500

    w2ep, globallly: initial chunk, which gets inherited by vendor chunk and all other chunks, i.e default

--- webpack.common.js
+++ webpack.common.js
@@ -41,46 +41,10 @@ module.exports = {
     new BundleAnalyzerPlugin()
   ],
   optimization: {
-    /*https://github.com/webpack/webpack/issues/10908*/
-    // minimize: false,
-
     splitChunks: {
-
-      /*https://medium.com/dailyjs/webpack-4-splitchunks-plugin-d9fbbe091fd0 to understand below property */
       chunks: 'initial',
-
-      /* Minimum size, in bytes, of a file to start splitting of that file and generate a chunk. Please note that minSize is calculated from the unminimized source code size */
-      minSize: 30000,
-
-      // minRemainingSize: 0,
-
-      // maxSize: 10000,
-
-      /*https://medium.com/jspoint/react-router-and-webpack-v4-code-splitting-using-splitchunksplugin-f0a48f110312 to understand below property */
-      // minChunks: 1,
-      // maxAsyncRequests: 30,
-      // maxInitialRequests: 30,
-      // automaticNameDelimiter: '~',
-      /* Size threshold at which splitting is enforced and other restrictions (minRemainingSize, maxAsyncRequests, maxInitialRequests, minSize) are ignored */
-      // enforceSizeThreshold: 5000000000,
-      // cacheGroups: {
-      //   vendor: {
-      //     chunks: 'async',
-      //     test: /node_modules/,
-      //     name: 'vendor'
-      //   }
-      // defaultVendors: {
-      //   test: /[\\/]node_modules[\\/]/,
-      //   priority: -10
-      // },
-      // default: {
-      //   minChunks: 2,
-      //   priority: -20,
-      //   reuseExistingChunk: true
-      // }
-    // }
+    }
   }
-},
   // optimization: {
   //   splitChunks: {
   //     cacheGroups: {
